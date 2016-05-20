020b3852585503e49b757f23d6d13930d61e8dcd
gnishida <gnishida@purdue.edu>
2016-05-20 15:33:01 -0400

    Adding Test4 (complete Sec 2.3)

--- Test4/main.cpp
+++ Test4/main.cpp
@@ -1,7 +1,6 @@
 ﻿/**
 * 論文Image-based procedural modeling of facadesの2.3節に基づいて、
 * Irreducible facadeを計算する。
-* ただし、水平方向のsplitのみ。
 *
 * @author Gen Nishida
 * @version 1.0
@@ -13,6 +12,32 @@
 #include <opencv2/opencv.hpp>
 
 using namespace std;
+
+/**
+ * IFデータに基づいて画像を生成する。
+ *
+ * @param	IF		IrreducibleFacade
+ * @param	imgIF	Image of IF
+ */
+void createIFImage(const cv::Mat& IF, cv::Mat& imgIF) {
+	imgIF = cv::Mat(IF.rows, IF.cols, CV_8UC3);
+
+	for (int r = 0; r < IF.rows; ++r) {
+		for (int c = 0; c < IF.cols; ++c) {
+			int blue = IF.at<cv::Vec4f>(r, c)[0] / IF.at<cv::Vec4f>(r, c)[3];
+			int green = IF.at<cv::Vec4f>(r, c)[1] / IF.at<cv::Vec4f>(r, c)[3];
+			int red = IF.at<cv::Vec4f>(r, c)[2] / IF.at<cv::Vec4f>(r, c)[3];
+			imgIF.at<cv::Vec3b>(r, c) = cv::Vec3b(blue, green, red);
+		}
+	}
+}
+
+void outputIF(const cv::Mat& IF, const string& filename) {
+	cv::Mat imgIF;
+	createIFImage(IF, imgIF);
+
+	cv::imwrite(filename, imgIF);
+}
 
 float MI(const cv::Mat& R1, const cv::Mat& R2) {
 	cv::Mat_<float> Pab(256, 256, 0.0f);
@@ -64,9 +89,42 @@ float MI(const cv::Mat& R1, const cv::Mat& R2) {
 }
 
 /**
- * y_initialの周辺で、最適なsplit位置、yを探す。 
- * その時のsimilarityを返却する。
+ * Irreducible Facadeの、[y, y+h]を[y-h, y]へ移動してshrinkさせる。
+ *
+ * @param	IF		Irreducible Facade
+ * @param	y		Splitの位置
+ * @param	h		Overlapさせる高さ
  */
+void shrinkIF(cv::Mat& IF, int y, int h) {
+	cv::Mat IF_bak = IF.clone();
+	IF = cv::Mat(IF.rows - h, IF.cols, CV_32FC4);
+
+	// overlapping部分より上側をコピー
+	for (int r = 0; r < y; ++r) {
+		for (int c = 0; c < IF.cols; ++c) {
+			IF.at<cv::Vec4f>(r, c) = IF_bak.at<cv::Vec4f>(r, c);
+		}
+	}
+
+	// overlapping部分をコピー
+	for (int r = y - h; r < y; ++r) {
+		for (int c = 0; c < IF.cols; ++c) {
+			IF.at<cv::Vec4f>(r, c) = IF.at<cv::Vec4f>(r, c) + IF_bak.at<cv::Vec4f>(r + h, c);
+		}
+	}
+
+	// overlapping部分より下側をコピー
+	for (int r = y; r < IF.rows; ++r) {
+		for (int c = 0; c < IF.cols; ++c) {
+			IF.at<cv::Vec4f>(r, c) = IF_bak.at<cv::Vec4f>(r + h, c);
+		}
+	}
+}
+
+/**
+* y_initialの周辺で、最適なsplit位置、yを探す。
+* その時のsimilarityを返却する。
+*/
 float findAdjacentSplitUpward(const cv::Mat_<float>& S_max, const cv::Mat_<int>& h_max, int y_initial, int h, int& y) {
 	y = y_initial;
 	float S = S_max(y, 0);
@@ -84,9 +142,9 @@ float findAdjacentSplitUpward(const cv::Mat_<float>& S_max, const cv::Mat_<int>&
 }
 
 /**
- * y_startから開始し、上方向へ、最適なsplit位置、next_yを探す。
- * その時のsimilarityを返却する。
- */
+* y_startから開始し、上方向へ、最適なsplit位置、next_yを探す。
+* その時のsimilarityを返却する。
+*/
 float findNextSplitUpward(const cv::Mat_<float>& S_max, const cv::Mat_<int>& h_max, int y_start, int& next_y) {
 	float S = 0.0f;
 
@@ -103,7 +161,7 @@ float findNextSplitUpward(const cv::Mat_<float>& S_max, const cv::Mat_<int>& h_m
 	return S;
 }
 
-void findSplitsUpward(const cv::Mat_<float>& S_max, const cv::Mat_<int>& h_max, int y, int h, float tau_max, vector<int>& y_set) {
+void findSplitsUpward(const cv::Mat_<float>& S_max, const cv::Mat_<int>& h_max, int y, int h, float tau_max, vector<int>& y_set, cv::Mat& IF) {
 	while (true) {
 		int next_y;
 		float S = findAdjacentSplitUpward(S_max, h_max, y, h, next_y);
@@ -121,13 +179,17 @@ void findSplitsUpward(const cv::Mat_<float>& S_max, const cv::Mat_<int>& h_max, 
 			if (S >= tau_max * 0.75f) {
 				y_set.push_back(next_y);
 				y = next_y - h_max(next_y, 0);
-				h = h_max(next_y, 0);
-				cout << "  --> modified. y: " << next_y << ", S: " << S << ", h: " << h_max(next_y, 0) << endl;
+				//cout << "  --> modified. y: " << next_y << ", S: " << S << ", h: " << h_max(next_y, 0) << endl;
 			}
 			else {
 				return;
 			}
 		}
+
+		// shrink IF
+		shrinkIF(IF, next_y, h_max(next_y, 0));
+
+		outputIF(IF, "IF3.png");
 	}
 }
 
@@ -139,6 +201,7 @@ int main() {
 	cv::Mat_<float> S_max(img.rows, 1, 0.0f);
 	cv::Mat_<int> h_max(img.rows, 1, 0.0f);
 
+	/*
 	printf("computing");
 	for (int r = 0; r < grayImg.rows; ++r) {
 		printf("\rcomputing r = %d/%d  ", r, grayImg.rows);
@@ -158,21 +221,50 @@ int main() {
 	printf("\n");
 
 	// output S_max(y) and h_max(y)
-	ofstream outS("S_max.txt");
+	ofstream out_S("S_max.txt");
 	for (int r = 0; r < S_max.rows; ++r) {
-		outS << S_max(r, 0) << endl;
+		out_S << S_max(r, 0) << endl;
 	}
-	outS.close();
-	ofstream outh("h_max.txt");
+	out_S.close();
+	ofstream out_h("h_max.txt");
 	for (int r = 0; r < h_max.rows; ++r) {
-		outh << h_max(r, 0) << endl;
+		out_h << h_max(r, 0) << endl;
 	}
-	outh.close();
+	out_h.close();
+	*/
+
+
+	ifstream in_S("S_max.txt");
+	for (int r = 0; r < S_max.rows; ++r) {
+		in_S >> S_max(r, 0);
+	}
+	in_S.close();
+	ifstream in_h("h_max.txt");
+	for (int r = 0; r < h_max.rows; ++r) {
+		in_h >> h_max(r, 0);
+	}
+	in_h.close();
+
 
 	// normalize S_max(y) to the range [0, 1]
 	cv::Mat S_max_max;
 	cv::reduce(S_max, S_max_max, 0, cv::REDUCE_MAX);
 	S_max /= S_max_max.at<float>(0, 0);
+
+	// initialize IF
+	cv::Mat IF(grayImg.rows, grayImg.cols, CV_32FC4);
+	for (int r = 0; r < IF.rows; ++r) {
+		for (int c = 0; c < IF.cols; ++c) {
+			cv::Vec4f v;
+			v[0] = img.at<cv::Vec3b>(r, c)[0];
+			v[1] = img.at<cv::Vec3b>(r, c)[1];
+			v[2] = img.at<cv::Vec3b>(r, c)[2];
+			v[3] = 1.0f;
+
+			IF.at<cv::Vec4f>(r, c) = v;
+		}
+	}
+	outputIF(IF, "IF1.png");
 
 	// find the maximum of S_max(y)
 	float S = 0.0f;
@@ -187,13 +279,17 @@ int main() {
 	vector<int> y_set;
 	y_set.push_back(y);
 
+	// shrink IF
+	shrinkIF(IF, y, h_max(y, 0));
+	outputIF(IF, "IF2.png");
+
 	cout << "y: " << y << ", S: " << S << ", h: " << h_max(y, 0) << endl;
 
 	// check upward
-	findSplitsUpward(S_max, h_max, y - h_max(y, 0), h_max(y, 0), S, y_set);
-	
-	cout << "Terminated." << endl;
+	findSplitsUpward(S_max, h_max, y - h_max(y, 0), h_max(y, 0), S, y_set, IF);
 
+	cout << "Terminated." << endl;
+	
 	// visualize S_max(y) and h_max(y)
 	cv::Mat result(img.rows, img.cols + 200 + 300, CV_8UC3, cv::Scalar(255, 255, 255));
 	for (int r = 0; r < img.rows; ++r) {
