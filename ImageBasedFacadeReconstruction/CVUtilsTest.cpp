517b15809a53fe60c24efc6179792ef7459c324e
gnishida <gnishida@purdue.edu>
2016-06-08 19:26:53 -0400

    Refactoring by adding facade_segmentation.cpp

--- ImageBasedFacadeReconstruction/CVUtilsTest.cpp
+++ ImageBasedFacadeReconstruction/CVUtilsTest.cpp
@@ -18,21 +18,29 @@ namespace cvutils {
 		A1.at<cv::Vec3f>(0, 1) = cv::Vec3f(2, 3, 1);
 		A1.at<cv::Vec3f>(1, 0) = cv::Vec3f(1, 1, 0);
 		A1.at<cv::Vec3f>(1, 1) = cv::Vec3f(3, 0, 1);
-		assert(max(A1) == 3.0);
+		if (max(A1) != 3.0) {
+			cerr << "test_max() failed at #1." << endl;
+		}
 
 		cv::Mat A2(2, 2, CV_8UC3);
 		A2.at<cv::Vec3b>(0, 0) = cv::Vec3b(0, 11, 2);
 		A2.at<cv::Vec3b>(0, 1) = cv::Vec3b(2, 3, 1);
 		A2.at<cv::Vec3b>(1, 0) = cv::Vec3b(21, 1, 0);
 		A2.at<cv::Vec3b>(1, 1) = cv::Vec3b(123, 40, 13);
-		assert(max(A2) == 123.0);
+		if (max(A2) != 123.0) {
+			cerr << "test_max() failed at #2." << endl;
+		}
 
 		cv::Mat A3 = (cv::Mat_<float>(3, 3) << 1, 3, 9, 4.5, 9.8, 3.2, 18.5, 20.5, 1.2);
-		assert(max(A3) == 20.5);
+		if (max(A3) != 20.5) {
+			cerr << "test_max() failed at #3." << endl;
+		}		
 
 		cv::Mat A4 = (cv::Mat_<unsigned char>(3, 3) << 21, 33, 94, 45, 98, 32, 185, 205, 12);
-		assert(max(A4) == 205.0);
-
+		if (max(A4) != 205.0) {
+			cerr << "test_max() failed at #4." << endl;
+		}
+		
 		cout << "test_max() done." << endl;
 	}
 
@@ -42,20 +50,29 @@ namespace cvutils {
 		A1.at<cv::Vec3f>(0, 1) = cv::Vec3f(2, 3, 1);
 		A1.at<cv::Vec3f>(1, 0) = cv::Vec3f(1, 1, 0);
 		A1.at<cv::Vec3f>(1, 1) = cv::Vec3f(3, 0, 1);
-		assert(min(A1) == -5.0);
+		if (min(A1) != -5.0) {
+			cerr << "test_min() failed at #1." << endl;
+		}
+
 
 		cv::Mat A2(2, 2, CV_8UC3);
 		A2.at<cv::Vec3b>(0, 0) = cv::Vec3b(0, 11, 2);
 		A2.at<cv::Vec3b>(0, 1) = cv::Vec3b(2, 3, 1);
 		A2.at<cv::Vec3b>(1, 0) = cv::Vec3b(21, 1, 0);
 		A2.at<cv::Vec3b>(1, 1) = cv::Vec3b(123, 40, 13);
-		assert(min(A2) == 0.0);
+		if (min(A2) != 0.0) {
+			cerr << "test_min() failed at #2." << endl;
+		}
 
 		cv::Mat A3 = (cv::Mat_<float>(3, 3) << 1, 3, 9, 4.5, 9.8, 3.2, 18.5, 20.5, 1.2);
-		assert(min(A3) == 1.0);
+		if (min(A3) != 1.0) {
+			cerr << "test_min() failed at #3." << endl;
+		}
 
 		cv::Mat A4 = (cv::Mat_<unsigned char>(3, 3) << 21, 33, 94, 45, 98, 32, 185, 205, 12);
-		assert(min(A4) == 12.0);
+		if (min(A4) != 12.0) {
+			cerr << "test_min() failed at #4." << endl;
+		}
 
 		cout << "test_min() done." << endl;
 	}
@@ -63,11 +80,15 @@ namespace cvutils {
 	void test_msd() {
 		cv::Mat A1 = (cv::Mat_<unsigned char>(3, 3) << 0, 1, 4, 2, 3, 5, 8, 1, 2);
 		cv::Mat A2 = (cv::Mat_<unsigned char>(3, 3) << 4, 0, 1, 2, 2, 1, 3, 4, 0);
-		assert(msd(A1, A2) == 9.0);
+		if (msd(A1, A2) != 9.0) {
+			cerr << "test_msd() failed at #1." << endl;
+		}
 
 		cv::Mat A3 = (cv::Mat_<float>(3, 3) << -1, 5, 3.2, 1.5, 0, 2, 4, 3.8, 0);
 		cv::Mat A4 = (cv::Mat_<float>(3, 3) << 3, 2, 1.2, 0.5, 4, 4, 2, 0.8, 0);
-		assert(msd(A3, A4) == 7.0);
+		if (msd(A3, A4) != 7.0) {
+			cerr << "test_msd() failed at #2." << endl;
+		}
 
 		cv::Mat A5(2, 2, CV_32FC3);
 		cv::Mat A6(2, 2, CV_32FC3);
@@ -79,7 +100,9 @@ namespace cvutils {
 		A6.at<cv::Vec3f>(0, 1) = cv::Vec3f(1, 0, 1);
 		A6.at<cv::Vec3f>(1, 0) = cv::Vec3f(3, 2, 1);
 		A6.at<cv::Vec3f>(1, 1) = cv::Vec3f(1, 0, 2);
-		assert(msd(A5, A6) == 6.0);
+		if (msd(A5, A6) != 6.0) {
+			cerr << "test_msd() failed at #3." << endl;
+		}
 
 		cv::Mat A7(2, 2, CV_8UC3);
 		cv::Mat A8(2, 2, CV_8UC3);
@@ -91,7 +114,9 @@ namespace cvutils {
 		A8.at<cv::Vec3b>(0, 1) = cv::Vec3b(1, 0, 1);
 		A8.at<cv::Vec3b>(1, 0) = cv::Vec3b(3, 2, 1);
 		A8.at<cv::Vec3b>(1, 1) = cv::Vec3b(1, 0, 2);
-		assert(msd(A7, A8) == 6.0);
+		if (msd(A7, A8) != 6.0) {
+			cerr << "test_msd() failed at #4." << endl;
+		}
 
 		cout << "test_msd() done." << endl;
 	}
@@ -99,12 +124,12 @@ namespace cvutils {
 	void test_corr() {
 		cv::Mat A1 = (cv::Mat_<unsigned char>(3, 3) << 0, 1, 4, 2, 3, 5, 5, 1, 3);
 		if (corr(A1, A1) != 1.0) {
-			cerr << "test_corr() failed #1." << endl;
+			cerr << "test_corr() failed at #1." << endl;
 		}
 
 		cv::Mat A2 = (cv::Mat_<unsigned char>(3, 3) << 1, 1, 2, 2, 2, 3, 4, 1, 0);
 		if (abs(corr(A1, A2) * 12.0 - 11.0) > 0.000001) {
-			cerr << "test_corr() failed #2." << endl;
+			cerr << "test_corr() failed at #2." << endl;
 		}
 
 		cout << "test_corr() done." << endl;
